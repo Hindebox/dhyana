1eee4798bf2262d2877dee298bde4996919dac3b
Hindebox <125393629+Hindebox@users.noreply.github.com>
2023-09-05 09:25:43 +0200

    Update Meditate.jsx

--- src/pages/meditate/Meditate.jsx
+++ src/pages/meditate/Meditate.jsx
@@ -1,4 +1,5 @@
 import "./meditate.scss";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import Loader from "../../components/loader/Loader";
 import Timer from "../../components/timer/Timer";
